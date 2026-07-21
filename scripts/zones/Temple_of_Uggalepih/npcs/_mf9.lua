620d69d7c0315f70066c3484e110bb5d9baade3d
djjohnson13p <60238197+djjohnson13p@users.noreply.github.com>
2026-07-21 10:34:03 -0700

    fix: require Uggalepih Key for western Map 2 door

--- scripts/zones/Temple_of_Uggalepih/npcs/_mf9.lua
+++ scripts/zones/Temple_of_Uggalepih/npcs/_mf9.lua
@@ -1,7 +1,7 @@
 -----------------------------------
 -- Area: Temple of Uggalepih
 --  NPC: _mf9 (Granite Door)
--- Notes: Opens with Prelate Key
+-- Notes: Opens with Uggalepih Key
 -- !pos -60 -8 -99 159
 -----------------------------------
 local ID = zones[xi.zone.TEMPLE_OF_UGGALEPIH]
@@ -10,16 +10,16 @@ local ID = zones[xi.zone.TEMPLE_OF_UGGALEPIH]
 local entity = {}
 
 entity.onTrade = function(player, npc, trade)
-    if npcUtil.tradeHas(trade, xi.item.PRELATE_KEY) then -- Prelate Key
+    if npcUtil.tradeHasExactly(trade, xi.item.UGGALEPIH_KEY) then
         player:confirmTrade()
-        player:messageSpecial(ID.text.YOUR_KEY_BREAKS, 0, xi.item.PRELATE_KEY)
+        player:messageSpecial(ID.text.YOUR_KEY_BREAKS, 0, xi.item.UGGALEPIH_KEY)
         npc:openDoor(6.5)
     end
 end
 
 entity.onTrigger = function(player, npc)
     if player:getXPos() > -62 then
-        player:messageSpecial(ID.text.THE_DOOR_IS_LOCKED, xi.item.PRELATE_KEY)
+        player:messageSpecial(ID.text.THE_DOOR_IS_LOCKED, xi.item.UGGALEPIH_KEY)
     else
         npc:openDoor(11) -- retail timed
     end
